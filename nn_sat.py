27029151d20bc179d1396cebb8492ceeda1b95f0
August Johnson Palm <august.palm.music@gmail.com>
2023-02-24 15:56:09 +0100

    completed all clause generators

--- nn_sat.py
+++ nn_sat.py
@@ -139,7 +139,7 @@ def relate_w_i(c, m, k):
                 formula.append([i_str[i_key], -w_str[w_key]])
                 formula.append([-i_str[i_key], w_str[w_key]])
 
-# relate_w_i(C, m, k)
+relate_w_i(C, m, k)
 # print(formula.clauses)
 
 
@@ -157,7 +157,7 @@ def relate_w_o(c, m):
             formula.append([-w_str[w_key], o_str[o_key]])
 
 
-# relate_w_o(C, m)
+relate_w_o(C, m)
 
 # print(formula.clauses)
 
@@ -184,7 +184,7 @@ def relate_y_omega_o(c, m, max):
                             [-y_str[y_key], -omega_str[omega_key], o_str[o_key]])
 
 
-# relate_y_omega_o(C, m, max)
+relate_y_omega_o(C, m, max)
 # print(formula.clauses)
 # print("Y:", y_str)
 # print()
@@ -205,7 +205,7 @@ def uniqueness_y(c, m, k, max):
                             formula.append([y_str[y_key_1], -y_str[y_key_2]])
 
 
-# uniqueness_y(C, m, k, max)
+uniqueness_y(C, m, k, max)
 
 # print(formula.clauses)
 
@@ -219,122 +219,69 @@ def uniqueness_omega(c, max):
                 if v != v_prime:
                     omega_key_1 = "omega" + str(a + 1) + str(v)
                     omega_key_2 = "omega" + str(a + 1) + str(v_prime)
-                    formula.append([omega_str[omega_key_1], -omega_str[omega_key_2]])
+                    formula.append(
+                        [omega_str[omega_key_1], -omega_str[omega_key_2]])
+
 
 uniqueness_omega(C, max)
 
-print(formula.clauses)
+# # print(formula.clauses)
 
-# #dataset
-# w = [
-#     [1, 2, 3],
-#     [-4, -5, -6],
-#     [-7, 8, -9]
-# ]
-
-# # outputs (length is n of strings in dataset)
-# o = [
-#     [10, -11, -12]
-# ]
-
-# # gate weigth / threshold
-# omega = [
-#     [-13, -14, 15, -16, -17, -18, -19, -20, -21, -22]
-# ]
-
-# # gate input
-# input = [
-#     [
-
-#         [23, 24],
-#         [-25, -26],
-#         [27, -28]
-#     ]
-# ]
-
-# # simulation variables
-# y = [
-#     [
-#         [-29, 30, -31, -32, -33, -34, -35, -36, -37, -38],
-#         [-39, -40, 41, -42, -43, -44, -45, -46, -47, -48]
-#     ],
-#     [
-#         [39, -40, -41, -42, -43, -44, -45, -46, -47, -48],
-#         [49, -50, -51, -52, -53, -54, -55, -56, -57, -58]
-#     ],
-#     [
-#         [-59, 60, -61, -62, -63, -64, -65, -66, -67, -68],
-#         [-69, 70, -71, -72, -73, -74, -75, -76, -77, -78]
-#     ],
-# ]
-
-# def get_data_entry(i):
-
-#     return w[i]
-
-# def get_output(i):
-#     return o[i]
-
-# def get_input_bit(gate, i, b):
-#     return input[gate][i][b]
-
-# def get_threshold(gate):
-#     for i in omega:
-#         if (omega[i] > 0):
-#             return i
+# print("Y: ", y_str)
+# print()
+# print("I: ", i_str)
+# print()
 
 
-# def formula(dataset):
+def relate_partial_sums_inputs_1(c, m, k, max):
+    for a in range(c):
+        for i in range(m):
+            for b in range(k):
+                for v in range(max):
+                    if b != k-1 and v != max-1:
+                        y_key_1 = "y" + str(a + 1) + \
+                            str(i + 1) + str(b+1) + str(v)
+                        y_key_2 = "y" + \
+                            str(a + 1) + str(i + 1) + \
+                            str(b+2) + str(v +1)
+                        i_key = "i" + str(a+1) + str(i + 1) + str(b+2)
+                        formula.append([-y_str[y_key_1], -i_str[i_key], y_str[y_key_2]])
 
-#     k = 2
+relate_partial_sums_inputs_1(C, m, k, max)
 
-#     solver = Glucose3()
-#     for i in range(len(dataset)):
-#         print("data:")
-#         print(i)
-#         for b in range(1, k):
-#                 print("i:")
-#                 print(i)
-#                 print("B:")
-#                 print(b-1)
-#                 solver.add_clause([input[0][i][b-1], dataset[i][b-1] * -1])
-#                 solver.add_clause([input[0][i][b-1] * -1, dataset[i][b-1]]),
-#                 solver.add_clause([w[i][0], o[0][i] * -1]),
-#                 solver.add_clause([w[i][0]*-1, o[0][i]]),
-#                 for v in range(len(omega[0])):
-#                     # add all clauses for when v' >= v. also add all clauses for when v != v' for the others
-#                     if (omega[0][v] > 0):
-#                         for v_, var in enumerate(y[i][k-1], start=v):
-#                             print("i:")
-#                             print(i)
-#                             print("k:")
-#                             print(k-1)
-#                             print("v_:")
-#                             print(v_)
-#                             print(y[i][k-1][v_])
-#                             #solver.add_clause(y[i][k-1][v_] * -1, omega[0][v])
-
-#         print("end")
-
-# formula(w)
+# print(formula.clauses)
 
 
-# # create a satisfiable CNF formula "(-x1 ∨ x2) ∧ (-x1 ∨ -x2)":
-# cnf = CNF(from_clauses=[[-1, 2], [-1, -2]])
+# print("Y: ", y_str)
+# print()
+# print("I: ", i_str)
+# print()
 
-# # create a SAT solver for this formula:
-# with Solver(bootstrap_with=cnf) as solver:
-#     # 1.1 call the solver for this formula:
-#     print('formula is', f'{"s" if solver.solve() else "uns"}atisfiable')
 
-#     # 1.2 the formula is satisfiable and so has a model:
-#     print('and the model is:', solver.get_model())
+def relate_partial_sums_inputs_2(c, m, k, max):
+    for a in range(c):
+        for i in range(m):
+            for b in range(k):
+                for v in range(max):
+                    if b != k-1:
+                        y_key_1 = "y" + str(a + 1) + \
+                            str(i + 1) + str(b+1) + str(v)
+                        y_key_2 = "y" + \
+                            str(a + 1) + str(i + 1) + \
+                            str(b+2) + str(v)
+                        i_key = "i" + str(a+1) + str(i + 1) + str(b+2)
+                        formula.append([y_str[y_key_1], -i_str[i_key], y_str[y_key_2]])
 
-#     # 2.1 apply the MiniSat-like assumption interface:
-#     print('formula is',
-#         f'{"s" if solver.solve(assumptions=[1, 2]) else "uns"}atisfiable',
-#         'assuming x1 and x2')
+relate_partial_sums_inputs_2(C, m, k, max)
 
-#     # 2.2 the formula is unsatisfiable,
-#     # i.e. an unsatisfiable core can be extracted:
-#     print('and the unsatisfiable core is:', solver.get_core())
+# print(formula.clauses)
+
+g = Glucose3()
+
+g.append_formula(formula)
+
+print(g.solve())
+
+print(g.get_model())
+
+
