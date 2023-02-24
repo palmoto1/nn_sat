597a4c7e149fc386e7d40004636b7792e56ac36c
August Johnson Palm <august.palm.music@gmail.com>
2023-02-24 14:25:52 +0100

    started generating clauses

--- nn_sat.py
+++ nn_sat.py
@@ -32,6 +32,15 @@ o_digit = {}
 omega_str = {}
 omega_digit = {}
 
+# gate input dicts
+i_str = {}
+i_digit = {}
+
+
+# simulation variable dicts
+y_str = {}
+y_digit = {}
+
 def generate_dataset(m, n):
     global w_str
     global w_digit
@@ -73,20 +82,98 @@ def generate_weights(c, max):
             count += 1
 
 
+def generate_gate_inputs(c, m, k):
+    global i_str
+    global i_digit
+    global count
+
+    for a in range(c):
+        for i in range(m):
+            for b in range(k):
+                s = "i" + str(a+1) + str(i+1) + str(b+1)
+                i_str[s] = count
+                i_digit[count] = s
+
+                count += 1
+
+
+def generate_simulation_variables(c, m, k, max):
+    global y_str
+    global y_digit
+    global count
+
+
+    for a in range(c):
+        for i in range(m):
+            for b in range(k):
+                for v in range(max):
+                    s = "y" + str(a+1) + str(i+1) + str(b+1) + str(v)
+                    y_str[s] = count
+                    y_digit[count] = s
+                    count += 1
+
+
+
+
+    
+
+
 
 
 
 generate_dataset(m, n)
 generate_outputs(C, m)
 generate_weights(C, max)
-print(omega_str)
-print(omega_digit)
+generate_gate_inputs(C, m, k)
+generate_simulation_variables(C, m, k, max)
 
-    # w = [
-#     [1, 2, 3],
-#     [-4, -5, -6],
-#     [-7, 8, -9]
-# ]
+
+
+formula = CNF()
+
+#print("w:", w_str)
+#print("i:", i_str)
+
+# create clauses relating dataset variables with gate input variables (should be equivalent)
+def relate_w_i(c, m, k):
+
+    for a in range(c):
+        for i in range(m):
+            for b in range(k):
+                w_key = "w" + str(i + 1) + str(b+1)
+                i_key = "i" + str (a+1) + str(i + 1) + str(b+1)
+                formula.append([i_str[i_key], -w_str[w_key]])
+                formula.append([-i_str[i_key], w_str[w_key]])
+
+#relate_w_i(C, m, k)
+#print(formula.clauses)
+
+
+print("w:", w_str)
+print("o:", o_str)
+
+
+# create clauses relating dataset labels with output variables (they should be equivalent)
+def relate_w_o(c, m):
+    for a in range(c):
+        for i in range(m):
+            w_key = "w" + str(i + 1) + str(0)
+            o_key = "o" + str (a+1) + str(i + 1)
+            formula.append([w_str[w_key], -o_str[o_key]])
+            formula.append([-w_str[w_key], o_str[o_key]])
+                
+
+
+
+
+relate_w_o(C, m)
+
+print(formula.clauses)
+  
+
+
+
+
 
 
 
